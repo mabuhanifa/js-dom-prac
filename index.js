23c7e9e9a1a4bc9051e1a954b921151d441d9c13
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-22 20:57:54 +0600

    btn functionality done

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 //dom elements
 const expenses = document.getElementById("expenses");
 const pExpenses = document.getElementById("pExpenses");
-const totalExpense = document.getElementById("totalExpense");
+const totalBtn = document.getElementById("totalBtn");
 const perPlayer = document.getElementById("budget");
 const manager = document.getElementById("manager");
 const coach = document.getElementById("coach");
+let total = document.getElementById("total");
+const playerList = document.getElementById("playerList");
 
 let selectedPlayer = [];
 let playerExpenses;
 console.log(selectedPlayer);
 
 function select(player) {
-  selectedPlayer.push(player);
+  if (selectedPlayer.includes(player)) {
+    alert(player + " is already selected");
+    return;
+  }
+  if (selectedPlayer.length < 5) {
+    selectedPlayer.push(player);
+  } else {
+    alert("Can not select more than 5player");
+    return;
+  }
+  const dynamicBtn = document.getElementById(player);
+  const li = document.createElement("li");
+  const textNode = document.createTextNode(player);
+  li.appendChild(textNode);
+  playerList.appendChild(li);
+  dynamicBtn.style.backgroundColor = "gray";
 }
 
 pExpenses.addEventListener("click", function pExpenses(event) {
   event.preventDefault();
   let budget = parseFloat(perPlayer.value);
-  expenses.innerHTML = budget * selectedPlayer.length;
+  playerExpenses = budget * selectedPlayer.length;
+  expenses.innerHTML = "$ " + budget * selectedPlayer.length;
   console.log(budget);
 });
 
-totalExpense.addEventListener("click", function totalExpense(event) {
+totalBtn.addEventListener("click", function totalExpense(event) {
   event.preventDefault();
+  let managerExpense = parseFloat(manager.value);
+  let coachExpense = parseFloat(coach.value);
+  let allExpense = managerExpense + coachExpense + playerExpenses;
+  total.innerHTML = "$ " + allExpense;
 });
